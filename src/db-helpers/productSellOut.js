fe9b256245de764d2a12e01100fc7a9675d2e97b
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2017-02-04 08:52:54 -0500

    Product sell out wasn't working, now it works

--- src/db-helpers/productSellOut.js
+++ src/db-helpers/productSellOut.js
@@ -1,23 +1,14 @@
 
 let sellOutProduct = async (db, { masterId }) => {
-  let query = {
-    masterId,
-    variants: {
-      $elemMatch: {
-        quantity: { $ne: 0 },
-      },
-    },
-  }
-
-  while (true) {
-    let cursor = await db.collection(`products`).find(query)
-    let products = await cursor.toArray()
-    if (!products.length)
-      break
-    await db.collection(`products`).update(
-      query,
-      { $set: { "variants.$.quantity": 0 } },
-      { multi: true }
+  let cursor = await db.collection(`products`).find({ masterId })
+  let products = await cursor.toArray()
+  if (products.length) {
+    let variants = products[0].variants
+    for(let sku of Object.keys(variants)) {
+      variants[sku] = { ...variants[sku], quantity: 0 }
+    }
+    await db.collection(`products`).update({ masterId },
+      { $set: { variants } },
     )
   }
 }
